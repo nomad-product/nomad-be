f6b7ee47b76bf050b1cd2131f66c4eaf01ed9fac
seyun(marco) <ksy90101@gmail.com>
2023-12-18 21:30:00 +0900

    refactor: rdb 값 변경

--- src/main/kotlin/com/nomad/nomad/common/configure/JasyptConfig.kt
+++ src/main/kotlin/com/nomad/nomad/common/configure/JasyptConfig.kt
@@ -1,5 +1,6 @@
 package com.nomad.nomad.common.configure
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties
 import org.jasypt.encryption.StringEncryptor
 import org.jasypt.encryption.pbe.PooledPBEStringEncryptor
 import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig
@@ -7,6 +8,7 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
+@EnableEncryptableProperties
 class JasyptConfig {
 
     @Bean("jasyptStringEncryptor")
