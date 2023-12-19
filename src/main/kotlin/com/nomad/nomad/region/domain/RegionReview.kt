f8118ac0800b02e448a8af8a18ff710666515955
SeYun(Marco) <ksy90101@gmail.com>
2023-12-19 21:14:15 +0900

    refactor: reviews 구현 (#5)

---
+++ src/main/kotlin/com/nomad/nomad/region/domain/RegionReview.kt
@@ -0,0 +1,32 @@
+package com.nomad.nomad.region.domain
+
+import com.nomad.nomad.common.entity.BaseEntity
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Lob
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.Table
+
+@Entity
+@Table(name = "region_reviews")
+class RegionReview: BaseEntity() {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null
+
+    @Column(name = "userName", nullable = false)
+    val userName: String? = null
+
+    @Column(name = "rating", nullable = false, length = 5)
+    val rating: Double? = null
+
+    @Lob
+    @Column(name = "content", nullable = false)
+    val content: String? = null
+
+    @ManyToOne
+    val provinceRegion: ProvinceRegion? = null
+}
